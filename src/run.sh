cff2e8599851d6fca54684c33989f19b7ea641dd
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-25 18:26:39 +0200

    First Start

---
+++ src/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mpirun -oversubscribe -np $1 tema3 $2 $3
--- src/tema3.c
+++ src/tema3.c
@@ -1,10 +1,39 @@
+#include "mpi.h"
+#include <stdio.h>
 #include <stdlib.h>
+ 
+#define  TOP_LEVEL 0
 
-int main() {
+#define NUMBER_OF_ARGUMENTS 3
 
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d %d", a, b);
 
-    return 0;
+int main (int argc, char *argv[]) {
+    int level;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &level);
+
+	if (argc != NUMBER_OF_ARGUMENTS) {
+		
+		if (level == TOP_LEVEL) {
+			printf("####################################\n");
+			printf("# Error: Wrong number of arguments #\n");
+			printf("####################################\n");
+		}
+		MPI_Finalize();
+		return EXIT_FAILURE;
+	}
+
+
+	for (int i = 0; i < 10; ++i) {
+		if (level == i) {
+			printf("Sunt aici : %d\n", level);
+		}
+	}
+
+
+ 
+    MPI_Finalize();
+
+	return EXIT_SUCCESS;
 }
