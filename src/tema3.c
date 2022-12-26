d6661b6df881fb667253bb12ea5905b2f49c27de
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-26 13:56:34 +0200

    Progress - 60P - Correct result created

--- src/tema3.c
+++ src/tema3.c
@@ -12,6 +12,9 @@
 #define SEND_DATA_SIZE_TAG 0
 #define SEND_DATA_TAG 1
 #define SEND_TOP_LEVEL_TAG 2
+#define SEND_START_INDEX_TAG 3
+#define SEND_END_INDEX_TAG 4
+#define DEFAULT_MULTIPLY_VALUE 5
 
 char *my_itoa(int number) {
 
@@ -137,7 +140,7 @@ void send_topology_to_workers(int** topology, int level) {
 }
 
 
-void receive_data_from_top_level(int **topology_data, int *top_level) {
+void receive_topology_from_top_level(int **topology_data, int *top_level) {
 
 	MPI_Recv(top_level, 1, MPI_INT, MPI_ANY_SOURCE, SEND_TOP_LEVEL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
@@ -149,7 +152,133 @@ void receive_data_from_top_level(int **topology_data, int *top_level) {
 	}
 }
 
+void init_and_send_work(int *result, int result_size, int *topology_at_level, int total_workers) {
+	for (int i = 0; i < result_size; ++i) {
+		result[i] = result_size - i - 1;
+	}
+	
+	int work_amount = result_size / total_workers;
+	int start = 0, end = work_amount - 1;
+	for (int i = 1; i <= topology_at_level[0]; ++i) {
+		MPI_Send(&start, 1, MPI_INT, topology_at_level[i], SEND_START_INDEX_TAG, MPI_COMM_WORLD);
+    	show_communication_message(TOP_LEVEL, topology_at_level[i]);
 
+		MPI_Send(&end, 1, MPI_INT, topology_at_level[i], SEND_END_INDEX_TAG, MPI_COMM_WORLD);
+    	show_communication_message(TOP_LEVEL, topology_at_level[i]);
+
+		MPI_Send(result, result_size, MPI_INT, topology_at_level[i], SEND_DATA_TAG, MPI_COMM_WORLD);
+    	show_communication_message(TOP_LEVEL, topology_at_level[i]);
+
+		start += work_amount;
+		end += work_amount;
+	}
+}
+
+void receive_result_from_top_level(int top_level, int result_size, int *result, int *start, int *end) {
+
+	MPI_Recv(start, 1, MPI_INT, top_level, SEND_START_INDEX_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(end, 1, MPI_INT, top_level, SEND_END_INDEX_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(result, result_size, MPI_INT, top_level, SEND_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+}
+
+
+void calculate_and_send_result_chunk(int level, int top_level, int result_size, int *result, int start, int end) {
+
+	for (int i = start; i <= end; ++i) {
+		result[i] *= DEFAULT_MULTIPLY_VALUE;
+	}
+
+	MPI_Send(&start, 1, MPI_INT, top_level, SEND_START_INDEX_TAG, MPI_COMM_WORLD);
+	show_communication_message(level, top_level);
+
+	MPI_Send(&end, 1, MPI_INT, top_level, SEND_END_INDEX_TAG, MPI_COMM_WORLD);
+	show_communication_message(level, top_level);
+
+	MPI_Send(result, result_size, MPI_INT, top_level, SEND_DATA_TAG, MPI_COMM_WORLD);
+	show_communication_message(level, top_level);
+
+
+}
+
+void receive_result_from_workers(int level, int *topology_at_level, int *result, int result_size) {
+
+	int start, end;
+	int *updated_result = (int *) malloc(result_size * sizeof(int));
+
+	for (int i = 1; i <= topology_at_level[0]; ++i) {
+
+		MPI_Recv(&start, 1, MPI_INT, topology_at_level[i], SEND_START_INDEX_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&end, 1, MPI_INT, topology_at_level[i], SEND_END_INDEX_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(updated_result, result_size, MPI_INT, topology_at_level[i], SEND_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		for (int j = start; j <= end; ++j) {
+			result[j] = updated_result[j];
+		}
+
+	}
+}
+
+void transfer_result(int source, int destination, int *result, int result_size) {
+
+	MPI_Send(result, result_size, MPI_INT, destination, SEND_DATA_TAG, MPI_COMM_WORLD);
+	show_communication_message(source, destination);
+
+}
+
+void receive_result(int source, int destination, int *result, int result_size) {
+
+	MPI_Recv(result, result_size, MPI_INT, destination, SEND_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+}
+
+void transfer_result_to_workers(int level, int *result, int result_size, int **topology_data, int total_workers, short last) {
+
+	int work_amount = result_size / total_workers;
+	int start = 0, end = 0;
+
+	int current_level = level;
+	int taken = 0;
+	while (current_level != TOP_LEVEL) {
+		++current_level;
+		if (current_level >= MAXIMUM_TOP_LEVEL) {
+			current_level = TOP_LEVEL;
+		}
+		taken += topology_data[current_level][0];
+	}
+
+	start = taken * work_amount;
+	end = start + work_amount - 1;
+
+	for (int i = 1; i <= topology_data[level][0]; ++i) {
+		if (last && i == topology_data[level][0]) {
+			end = result_size;
+		}
+		MPI_Send(&start, 1, MPI_INT, topology_data[level][i], SEND_START_INDEX_TAG, MPI_COMM_WORLD);
+    	show_communication_message(level, topology_data[level][i]);
+
+		MPI_Send(&end, 1, MPI_INT, topology_data[level][i], SEND_END_INDEX_TAG, MPI_COMM_WORLD);
+    	show_communication_message(level, topology_data[level][i]);
+
+		MPI_Send(result, result_size, MPI_INT, topology_data[level][i], SEND_DATA_TAG, MPI_COMM_WORLD);
+    	show_communication_message(level, topology_data[level][i]);
+
+		start += work_amount;
+		end += work_amount;
+	}
+
+}
+
+
+
+void show_final_result(int result_size, int *result) {
+
+	printf("Rezultat: ");
+	for (int i = 0; i < result_size; ++i) {
+		printf("%d ", result[i]);
+	}
+	printf("\n");
+}
 
 void write_topology(int **topology_data, int level) {
 
@@ -210,10 +339,68 @@ int main (int argc, char *argv[]) {
 	}
 
 	if (level >= MAXIMUM_TOP_LEVEL) {
-		receive_data_from_top_level(topology_data, &top_level);
+		receive_topology_from_top_level(topology_data, &top_level);
 	}
 	
 	write_topology(topology_data, level);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int result_size = atoi(argv[1]);
+	int error = atoi(argv[2]);
+	int *result = (int *) malloc(result_size * sizeof(int));
+
+	int total_workers = 0;
+	for (int i = 0; i < MAXIMUM_TOP_LEVEL; ++i) {
+		total_workers += topology_data[i][0];
+	}
+
+	if (level == TOP_LEVEL) {
+
+		init_and_send_work(result, result_size, topology_data[level], total_workers);
+		receive_result_from_workers(level, topology_data[level], result, result_size);
+		transfer_result(level, 3, result, result_size);
+		if (error != 0) {
+
+		}
+		else {
+			receive_result(level, 1, result, result_size);
+			show_final_result(result_size, result);
+		}
+		
+	}
+	else if (level < MAXIMUM_TOP_LEVEL) {
+
+		if (level == 3) {
+			receive_result(level, TOP_LEVEL, result, result_size);
+			transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 0);
+			receive_result_from_workers(level, topology_data[level], result, result_size);
+			transfer_result(level, 2, result, result_size);
+
+		}
+
+		if (level == 2) {
+			receive_result(level, 3, result, result_size);
+			transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 0);
+			receive_result_from_workers(level, topology_data[level], result, result_size);
+			transfer_result(level, 1, result, result_size);
+		}
+
+		if (level == 1) {
+			receive_result(level, 2, result, result_size);
+			transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 1);
+			receive_result_from_workers(level, topology_data[level], result, result_size);
+			transfer_result(level, 0, result, result_size);
+		}
+
+	}
+	else {
+
+		int start, end;
+		receive_result_from_top_level(top_level, result_size, result, &start, &end);
+		calculate_and_send_result_chunk(level, top_level, result_size, result, start, end);
+	}
+
 
 	
  
