3e25cf845d0119d0d4a318bd96248234614285c7
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-12-26 22:09:10 +0200

    FINISHED

--- README.md
+++ README.md
@@ -1,92 +1,49 @@
-# tema3
+<Copyright Alexandru Olteanu, grupa 332CA, alexandruolteanu2001@gmail.com>
 
+    In implementarea acestei teme am folosit MPI-uri pentru ca crea si
+implementa procesele de la nivelul programului. Asa cum a fost precizat
+in cerinta, am impartit procesele in doua categorii: manageri si workeri.
+    Cerintele au fost rezolvate in modul urmator:
+I. Afisarea topologiei de catre toate procesele.
+    Pentru inceput am aflat topologia fiecarui cluster in parte prin 
+parcurgerea fisierelor corespunzatoare de catre procesele manager. 
+Dupa realizarea citirii am trimis datele intre procesele manager urmarind
+configuratia de inel. Fiind 4 manageri, unele informatii au trebuit transportate
+prin noduri intermediare, astfel ca m-am asigurat ca trecerea datelor se face 
+cronologic astfel incat mereu managerul unui cluster are informatiile care i se 
+cer la un moment dat. Dupa trimiterea datelor intre manageri mai ramane ca 
+workerii sa stie topologia. Pentru a putea informa un worker in legatura cu 
+topologia a trebuit sa o primeasca de la coordonatorul sau. O problema este 
+ca workerii nu stiu ce coordonator au asa ca la operatia de MPI_Recv a topologiei
+a trebuit sa deschidem o ascultare de la toate nodurile si astfel sa primeasca 
+mesajul corect. Pentru a fi si informat de coordonatorul sau, ID-ul acestuia 
+i-a fost trimis in interiorul mesajului. Odata ce toate procesele au aflat 
+topologia au afisat-o. De asemenea, la fiecare trimitere de mesaje intre doua
+procese, au fost afisate in log.
 
+II. Pentru calculul vectorului am facut initializarea in cadrul procesului 0 
+dupa care am trimis date fiecarui worker din cadrul acestuia si un interval
+pe care fiecare din ei trebuie sa il rezolve. Intervalul a fost repartizat
+aproximativ egal in functie de numarul total de procese worker. La randul lor, 
+aceste procese au realizat inmultirea vectorului cu 5 in intervalul specificat 
+dupa care au raspuns inapoi coordonatorului cu datele updatate. Dupa ce un 
+coordonator a strans toate datele de la workeri le trimite mai departe 
+pe ruta urmatoare -> 0 -> 3 -> 2 -> 1 -> 0. Astfel, cand datele ajung a doua oara
+la procesul 0, vectorul este rezolvat complet si poate fi afisat.
 
-## Getting started
+III. In cadrul unei erori de comunicare intre procesele 0 si 1 am realizat 
+intreaga comunicare dar cu o ruta modificata astfel incat datele sa ajunga
+in continuare in mod corect. Spre exemplu, in aflarea topologiei, datele lui
+0 trebuiau sa parcurga drumul 0 -> 3 -> 2 -> 1 pentru a ajunge la 1 in loc 
+de ruta 0 -> 1 care era posibila initial. De asemenea, pentru calcularea rezultatelor
+am urmat ruta 0 -> 3 -> 2 -> 1 -> 2 -> 3 -> 0 care are acelasi rezultat, tranzitiile
+din urma avand ca rol doar transferul de date.
 
-To make it easy for you to get started with GitLab, here's a list of recommended next steps.
+IV. In cadrul bonusului am considerat procesul 1 total separat de celelalte astfel ca
+atat rutele s-au schimbat cat si topologiile afisate. Astfel, s-au afisat doua tipuri
+de topologii, cea a lui 0, 2, 3, si cea a lui 1. Avand in vedere aceste modificari dese
+ale rutelor am incercat sa realizez o abordare cat mai generica care sa poate fi modificata
+in viitor in functie de noi reguli.
 
-Already a pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
-
-## Add your files
-
-- [ ] [Create](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#create-a-file) or [upload](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#upload-a-file) files
-- [ ] [Add files using the command line](https://docs.gitlab.com/ee/gitlab-basics/add-file.html#add-a-file-using-the-command-line) or push an existing Git repository with the following command:
-
-```
-cd existing_repo
-git remote add origin https://gitlab.cs.pub.ro/apd/tema3.git
-git branch -M main
-git push -uf origin main
-```
-
-## Integrate with your tools
-
-- [ ] [Set up project integrations](https://gitlab.cs.pub.ro/apd/tema3/-/settings/integrations)
-
-## Collaborate with your team
-
-- [ ] [Invite team members and collaborators](https://docs.gitlab.com/ee/user/project/members/)
-- [ ] [Create a new merge request](https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html)
-- [ ] [Automatically close issues from merge requests](https://docs.gitlab.com/ee/user/project/issues/managing_issues.html#closing-issues-automatically)
-- [ ] [Enable merge request approvals](https://docs.gitlab.com/ee/user/project/merge_requests/approvals/)
-- [ ] [Automatically merge when pipeline succeeds](https://docs.gitlab.com/ee/user/project/merge_requests/merge_when_pipeline_succeeds.html)
-
-## Test and Deploy
-
-Use the built-in continuous integration in GitLab.
-
-- [ ] [Get started with GitLab CI/CD](https://docs.gitlab.com/ee/ci/quick_start/index.html)
-- [ ] [Analyze your code for known vulnerabilities with Static Application Security Testing(SAST)](https://docs.gitlab.com/ee/user/application_security/sast/)
-- [ ] [Deploy to Kubernetes, Amazon EC2, or Amazon ECS using Auto Deploy](https://docs.gitlab.com/ee/topics/autodevops/requirements.html)
-- [ ] [Use pull-based deployments for improved Kubernetes management](https://docs.gitlab.com/ee/user/clusters/agent/)
-- [ ] [Set up protected environments](https://docs.gitlab.com/ee/ci/environments/protected_environments.html)
-
-***
-
-# Editing this README
-
-When you're ready to make this README your own, just edit this file and use the handy template below (or feel free to structure it however you want - this is just a starting point!). Thank you to [makeareadme.com](https://www.makeareadme.com/) for this template.
-
-## Suggestions for a good README
-Every project is different, so consider which of these sections apply to yours. The sections used in the template are suggestions for most open source projects. Also keep in mind that while a README can be too long and detailed, too long is better than too short. If you think your README is too long, consider utilizing another form of documentation rather than cutting out information.
-
-## Name
-Choose a self-explaining name for your project.
-
-## Description
-Let people know what your project can do specifically. Provide context and add a link to any reference visitors might be unfamiliar with. A list of Features or a Background subsection can also be added here. If there are alternatives to your project, this is a good place to list differentiating factors.
-
-## Badges
-On some READMEs, you may see small images that convey metadata, such as whether or not all the tests are passing for the project. You can use Shields to add some to your README. Many services also have instructions for adding a badge.
-
-## Visuals
-Depending on what you are making, it can be a good idea to include screenshots or even a video (you'll frequently see GIFs rather than actual videos). Tools like ttygif can help, but check out Asciinema for a more sophisticated method.
-
-## Installation
-Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
-
-## Usage
-Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
-
-## Support
-Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
-
-## Roadmap
-If you have ideas for releases in the future, it is a good idea to list them in the README.
-
-## Contributing
-State if you are open to contributions and what your requirements are for accepting them.
-
-For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
-
-You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
-
-## Authors and acknowledgment
-Show your appreciation to those who have contributed to the project.
-
-## License
-For open source projects, say how it is licensed.
-
-## Project status
-If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+    Per total, o tema buna ce a abordat scopul materiei corespunzator, thx!
+        
--- src/tema3.c
+++ src/tema3.c
@@ -1,9 +1,12 @@
+/**
+ *  <Copyright Alexandru Olteanu, grupa 332CA, alexandruolteanu2001@gmail.com>
+ * */
 #include "mpi.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
  
-#define  TOP_LEVEL 0
+#define TOP_LEVEL 0
 #define NUMBER_OF_ARGUMENTS 3
 #define MAXIMUM_TOP_LEVEL  4
 #define MAXIMUM_LOW_LEVEL 100
@@ -18,7 +21,10 @@
 #define CONNECTION_ERROR 1
 #define PARTITION_ERROR 2
 
-
+/**
+ * Functie ce transforma un numar intr-un sir de caractere, folosita pentru numele 
+ * fisierelor de input
+ * */
 char *my_itoa(int number) {
 
 	if (!number) {
@@ -35,7 +41,10 @@ char *my_itoa(int number) {
 	return result;
 }
 
-
+/**
+ * In functie de cluserul in care suntem extragem numele fisierelor si cititm 
+ * procesele worker ale coordonatorului curent
+ * */
 void extract_data(int *top_level_workers, int level) {
 
 	char file_name[FILE_NAME_LENGTH];
@@ -52,13 +61,18 @@ void extract_data(int *top_level_workers, int level) {
 	}
 }
 
+/**
+ * Afiseaza notificarea cu privire la trimiterea unui mesaj de la un proces la altul
+ * */
 void show_communication_message(int source, int destination) {
 
 	printf("M(%d,%d)\n", source, destination);
 
 }
 
-
+/**
+ * Functie care trimite topologia procesului "level_of_data" de la procesul "source" la procesul "destination"
+ * */
 void send_data_to_top_level(int source, int destination, int level_of_data, int **topology_data) {
 
 	MPI_Send(&topology_data[level_of_data][0], 1, MPI_INT, destination, SEND_DATA_SIZE_TAG, MPI_COMM_WORLD);
@@ -68,6 +82,9 @@ void send_data_to_top_level(int source, int destination, int level_of_data, int 
 	show_communication_message(source, destination);
 }
 
+/**
+ * Functie care primeste topologia procesului "level_of_data" de la procesul "destination" la procesul "source"
+ * */
 void receive_data_from_top_level(int source, int destination, int level_of_data, int **topology_data) {
 
 	int data_size;
@@ -77,6 +94,10 @@ void receive_data_from_top_level(int source, int destination, int level_of_data,
 
 }
 
+/**
+ * Functie in care un coordonator parcurge lista de workeri din interiorul clusterului sau si le trimite 
+ * topologia fiecaruia dintre ei.
+ * */
 void send_topology_to_workers(int **topology, int level) {
   	int workers = topology[level][0];
   	for (int i = 1; i <= workers; ++i) {
@@ -101,6 +122,9 @@ void send_topology_to_workers(int **topology, int level) {
   	}
 }
 
+/**
+ * Functie in care un proces worker primeste topologia sistemului de la coordonatorul sau. 
+ * */
 void receive_topology_from_top_level(int **topology_data, int *top_level) {
 
 	MPI_Recv(top_level, 1, MPI_INT, MPI_ANY_SOURCE, SEND_TOP_LEVEL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -116,6 +140,10 @@ void receive_topology_from_top_level(int **topology_data, int *top_level) {
 	}
 }
 
+/**
+ * Functie destinata procesului 0 care initializeaza vectorul rezultat si trimite task-uri proceselor
+ * worker din cadrul clusterului sau.
+ * */
 void init_and_send_work(int *result, int result_size, int *topology_at_level, int total_workers) {
 	for (int i = 0; i < result_size; ++i) {
 		result[i] = result_size - i - 1;
@@ -138,6 +166,10 @@ void init_and_send_work(int *result, int result_size, int *topology_at_level, in
 	}
 }
 
+/**
+ * Functie in care un proces worker priemste vectorul rezultat de la coordonatorul sau impreuna cu 
+ * intervalul pe care il are de calculat
+ * */
 void receive_result_from_top_level(int top_level, int result_size, int *result, int *start, int *end) {
 
 	MPI_Recv(start, 1, MPI_INT, top_level, SEND_START_INDEX_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -146,7 +178,10 @@ void receive_result_from_top_level(int top_level, int result_size, int *result, 
 
 }
 
-
+/**
+ * Functie in care un proces worker realizeaza calculul pe intervalul corespunzator si trimite apoi datele
+ * inapoi catre coordonatorul sau
+ * */
 void calculate_and_send_result_chunk(int level, int top_level, int result_size, int *result, int start, int end) {
 
 	for (int i = start; i <= end; ++i) {
@@ -165,6 +200,9 @@ void calculate_and_send_result_chunk(int level, int top_level, int result_size, 
 
 }
 
+/**
+ * Functie in care un coordonator primeste vectorul rezultat dupa task-ul finalizat al unui worker
+ * */
 void receive_result_from_workers(int level, int *topology_at_level, int *result, int result_size) {
 
 	int start, end;
@@ -183,6 +221,9 @@ void receive_result_from_workers(int level, int *topology_at_level, int *result,
 	}
 }
 
+/**
+ * Functie in care doi coordonatori comunica intre ei, "source" trimite vectorul rezultat catre "destination"
+ * */
 void transfer_result(int source, int destination, int *result, int result_size) {
 
 	MPI_Send(result, result_size, MPI_INT, destination, SEND_DATA_TAG, MPI_COMM_WORLD);
@@ -190,12 +231,21 @@ void transfer_result(int source, int destination, int *result, int result_size) 
 
 }
 
+/**
+ * Functie in care un proces coordonator priemste vectorul rezultat de la un altul
+ * */
 void receive_result(int source, int destination, int *result, int result_size) {
 
 	MPI_Recv(result, result_size, MPI_INT, destination, SEND_DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 }
 
+
+/**
+ * Functie in care un alt proces coordonator diferit de 0 trimite vectorul rezultat catre workerii sai, calculand
+ * corespunzator intervalul pe care acesta are sa lucreze. In cazul in care acesta un coordonator este ultimul
+ * responsabil sa imparta task-uri, acesta da ultimului worker si restul de interval ramas ca rest la impartire.
+ * */
 void transfer_result_to_workers(int level, int *result, int result_size, int **topology_data, int total_workers, short last) {
 
 	int work_amount = result_size / total_workers;
@@ -233,8 +283,9 @@ void transfer_result_to_workers(int level, int *result, int result_size, int **t
 
 }
 
-
-
+/**
+ * Functie ce afiseaza rezultatul de catre procesul 0 odata ce este calculat.
+ **/ 
 void show_final_result(int result_size, int *result) {
 
 	printf("Rezultat: ");
@@ -244,6 +295,9 @@ void show_final_result(int result_size, int *result) {
 	printf("\n");
 }
 
+/**
+ * Functie ce afiseaza topologia dintr-un anumit proces, fie el coordonator sau worker.
+ * */
 void write_topology(int **topology_data, int level) {
 
 	printf("%d -> ", level);
@@ -268,6 +322,7 @@ void write_topology(int **topology_data, int level) {
 int main (int argc, char *argv[]) {
     int level;
 
+	// Realizam initializarea MPI-urilor
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &level);
 
@@ -282,10 +337,13 @@ int main (int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
+	// Obtinem dimensiunea vectorului result, codul de eroare si alocam memorie pentru result.
 	int result_size = atoi(argv[1]);
 	int error = atoi(argv[2]);
 	int *result = (int *) malloc(result_size * sizeof(int));
 
+	// Alocam memorie pentru topologie si initializam fiecare linie cu NULL (va fi alocata cand se primesc
+	// date catre acea linie)
 	int **topology_data = (int **) malloc(MAXIMUM_TOP_LEVEL * sizeof(int *));
 	for (int i = 0; i < MAXIMUM_TOP_LEVEL; ++i) {
 		topology_data[i] = NULL;
@@ -295,6 +353,8 @@ int main (int argc, char *argv[]) {
 
 	int top_level;
 	if (level < MAXIMUM_TOP_LEVEL) {
+		// Cand suntem intr-un proces coordonator extragem datele despre clusterul curent
+		// din fisier si alocam memorie in topologie pentru aceste date
 		extract_data(top_level_workers, level);
 		int workers_number = top_level_workers[0];
 		topology_data[level] = (int *) malloc((workers_number + 1) * sizeof(int));
@@ -303,6 +363,7 @@ int main (int argc, char *argv[]) {
 		}
 
 		if (error == NO_ERROR) {
+			// Construim rutele si transferam date despre topologie in cazul in care nu avem eroare de comunicare
 			if (level == 0) {
 				send_data_to_top_level(0, 1, 0, topology_data);
 				send_data_to_top_level(0, 3, 0, topology_data);
@@ -335,7 +396,7 @@ int main (int argc, char *argv[]) {
 			}
 		}
 		else if (error == CONNECTION_ERROR) {
-			 
+			// Construim rutele si transferam date despre topologie in cazul in care procesul 0 si 1 nu pot comunica
 			 if (level == 0) {
 				send_data_to_top_level(0, 3, 0, topology_data);
 				receive_data_from_top_level(0, 3, 3, topology_data);
@@ -368,6 +429,7 @@ int main (int argc, char *argv[]) {
 			}
 
 		} else {
+			// Construim rutele si transferam date despre topologie, ignorand complet procesul 1
 			if (level == 0) {
 				send_data_to_top_level(0, 3, 0, topology_data);
 				receive_data_from_top_level(0, 3, 3, topology_data);
@@ -387,16 +449,19 @@ int main (int argc, char *argv[]) {
 				send_data_to_top_level(3, 2, 0, topology_data);
 			}
 		}
-
+		// In acest moment coordonatorul are intreaga topologie si o poate trimite workerilor sai
 		send_topology_to_workers(topology_data, level);
 	}
-
+	// Daca avem un proces worker acesta va primi topologia de la coordonatorul sau
 	if (level >= MAXIMUM_TOP_LEVEL) {
 		receive_topology_from_top_level(topology_data, &top_level);
 	}
 	
+	// Toate procesele cunosc topologia asa ca este afisata de fiecare proces in parte
 	write_topology(topology_data, level);
 
+	// Calculam numarul total de workeri din topologie pentru a putea face distribuirea 
+	// corecta a task-urilor catre workeri
 	int total_workers = 0;
 	for (int i = 0; i < MAXIMUM_TOP_LEVEL; ++i) {
 		if (topology_data[i] == NULL) {
@@ -407,9 +472,14 @@ int main (int argc, char *argv[]) {
 
 	if (level == TOP_LEVEL) {
 
+		// Procesul 0 initializeaza vectorul result si trimite task-uri workerilor sai 
 		init_and_send_work(result, result_size, topology_data[level], total_workers);
+		// Primeste rezultatul de la acestia
 		receive_result_from_workers(level, topology_data[level], result, result_size);
+		// Transfera rezultatul mai departe catre procesul 3
 		transfer_result(level, 3, result, result_size);
+		// In cazul unei erori de comunicare cu procesul 1 acesta primeste rezultatul pe ruta pe care a plecat, daca nu, 
+		// prin intermediul procesului 1
 		if (error != NO_ERROR) {
 			receive_result(level, 3, result, result_size);
 			show_final_result(result_size, result);
@@ -423,6 +493,9 @@ int main (int argc, char *argv[]) {
 	else if (level < MAXIMUM_TOP_LEVEL) {
 
 		if (level == 3) {
+			// Pentru procesul 3 acesta priemste rezultatul de la procesul 0, trimite task-uri catre workeri si il transfera
+			// apoi mai departe catre 2. Apoi, in functie de prezenta unei erori de comunicare sau partitionare, asteapta fie 
+			// rezultatul inapoi pentru a-l da mai departe lui 0 ori nu mai asteapta date.
 			if (error == NO_ERROR) {
 				receive_result(level, TOP_LEVEL, result, result_size);
 				transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 0);
@@ -442,6 +515,8 @@ int main (int argc, char *argv[]) {
 		}
 
 		if (level == 2) {
+			// La fel ca procesul 3, acesta trimite mai departe in lantul normal al rutei si asteapta raspuns inapoi de la 
+			// 1 daca avem eroare de comunicare.
 			if (error == NO_ERROR) {
 				receive_result(level, 3, result, result_size);
 				transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 0);
@@ -465,6 +540,8 @@ int main (int argc, char *argv[]) {
 		}
 
 		if (level == 1) {
+			// Procesul 1 procedeaza asemenator, doar ca in cazul unei erori de partitionari acesta nu mai este prezent
+			// in realizarea calulelor.
 			if (error == NO_ERROR) {
 				receive_result(level, 2, result, result_size);
 				transfer_result_to_workers(level, result, result_size, topology_data, total_workers, 1);
@@ -480,6 +557,7 @@ int main (int argc, char *argv[]) {
 	}
 	else {
 
+		// Procesele worker asteaapta acum sa primeasca task-uri, sa le rezolve si sa le trimita inapoi
 		int start, end;
 		if (error != PARTITION_ERROR || top_level != 1) {
 			receive_result_from_top_level(top_level, result_size, result, &start, &end);
